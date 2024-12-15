f76834693392f422d66cc51ddf2c8bff032fdc61
Sabin <sabintheworld@gmail.com>
2024-12-14 21:11:00 -0800

    Binds host so I can access on local network

--- apps/websocket-gateway/src/main.ts
+++ apps/websocket-gateway/src/main.ts
@@ -3,6 +3,6 @@ import { WebsocketGatewayModule } from './websocket-gateway.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(WebsocketGatewayModule);
-	await app.listen(5004);
+	await app.listen(5004, '0.0.0.0');
 }
 bootstrap();
