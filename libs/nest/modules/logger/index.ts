80ac2a2b356bb17b8bb4465281150dca2c92a7ff
Sabin Adams <sabintheworld@gmail.com>
2025-08-06 14:29:00 -0700

    cleanup: remove unused NatsConsumerBase class
    - Remove NatsConsumerBase class since app-level interceptors make it unnecessary
    - Update logger module exports to remove the deleted class
    - All NATS consumers now use automatic app-level interceptor configuration
    
    The distinction between requestId (for HTTP/gRPC requests) and correlationId
    (for NATS events) is both standard practice and semantically correct.

--- libs/nest/modules/logger/index.ts
+++ libs/nest/modules/logger/index.ts
@@ -3,5 +3,4 @@ export * from './logger.service';
 export * from './loki-transport.service';
 export * from './nats-request-id.interceptor';
 export * from './grpc-request-id.interceptor';
-export * from './nats-consumer.base';
 export * from './request-tracing.module';
